84a046476643913a16a10f19c3278a67db1057a5
macnick <nick@macexperts.gr>
2020-04-27 19:48:07 +0300

    Fix calculator object and tests

---
+++ calculator/calculator.js
@@ -0,0 +1,10 @@
+const calculator = (() => {
+  return {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    divide: (a, b) => a / b,
+    multiply: (a, b) => a * b,
+  };
+})();
+
+module.exports = calculator;
---
+++ calculator/calculator.test.js
@@ -0,0 +1,17 @@
+const calculator = require('./calculator');
+
+test('Should do addition', () => {
+  expect(calculator.add(2, 3)).toEqual(5);
+});
+
+test('Should do subtraction', () => {
+  expect(calculator.subtract(2, 3)).toEqual(-1);
+});
+
+test('Should do division', () => {
+  expect(calculator.divide(2, 3)).toBeCloseTo(0.66666);
+});
+
+test('Should do multiplication', () => {
+  expect(calculator.multiply(2, 3)).toEqual(6);
+});
