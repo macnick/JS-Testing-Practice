75129a4f6969d05d26ba47213bb8657e8114dacb
macnick <nick@macexperts.gr>
2020-04-29 11:33:08 +0300

    Fix eslint errors

--- caesarChipher/caesarCipher.js
+++ caesarChipher/caesarCipher.js
@@ -1,10 +1,7 @@
-const caesarChipher = (str, num) =>
-  str
-    .replace(/[a-z]/g, (m) =>
-      String.fromCharCode(((m.charCodeAt(0) + num - 97) % 26) + 97)
-    )
-    .replace(/[A-Z]/g, (m) =>
-      String.fromCharCode(((m.charCodeAt(0) + num - 65) % 26) + 65)
-    );
+const caesarChipher = (str, num) => str.replace(/[a-z]/g, (m) =>
+  String.fromCharCode(((m.charCodeAt(0) + num - 97) % 26) + 97)
+).replace(/[A-Z]/g, (m) =>
+  String.fromCharCode(((m.charCodeAt(0) + num - 65) % 26) + 65)
+);
 
 export default caesarChipher;
