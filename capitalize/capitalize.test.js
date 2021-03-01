562ebb2cd69ac54d62688d9f5aa19ef3fecbf165
macnick <nick@macexperts.gr>
2021-03-01 14:17:03 +0200

    Fix linter error

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -2,7 +2,7 @@ import capitalize from './capitalize';
 
 test('The first letter of each word should be capital', () => {
   expect(capitalize('welcome to javascript testing')).toMatch(
-    'Welcome To Javascript Testing'
+    'Welcome To Javascript Testing',
   );
   expect(capitalize(' nick haras')).toMatch('Nick Haras');
 });
