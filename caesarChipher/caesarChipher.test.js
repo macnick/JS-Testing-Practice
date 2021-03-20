3adfa9aedbb4cd26ad9cf54c6a766aaf69ac74a8
Nick Haras <nick@macexperts.gr>
2021-03-20 19:18:51 +0200

    Add edge case tests for caesarChipher

--- caesarChipher/caesarChipher.test.js
+++ caesarChipher/caesarChipher.test.js
@@ -1,13 +1,21 @@
-import caesarChipher from './caesarCipher';
+import caesarChipher from "./caesarCipher";
 
-test('Should wrap from z to a', () => {
-  expect(caesarChipher('hello world', 4)).toMatch('lipps asvph');
+test("Should wrap from z to a", () => {
+  expect(caesarChipher("hello world", 4)).toMatch("lipps asvph");
 });
 
-test('Should keep the same case', () => {
-  expect(caesarChipher('Hello World', 4)).toMatch('Lipps Asvph');
+test("Should keep the same case", () => {
+  expect(caesarChipher("Hello World", 4)).toMatch("Lipps Asvph");
 });
 
-test('Should not change punctuation', () => {
-  expect(caesarChipher('Hello, World!', 4)).toMatch('Lipps, Asvph!');
+test("Should not change punctuation", () => {
+  expect(caesarChipher("Hello, World!", 4)).toMatch("Lipps, Asvph!");
+});
+
+test("Should work for empty strings", () => {
+  expect(caesarChipher("", 2)).toMatch("");
+});
+
+test("Should return the same string if n is zero", () => {
+  expect(caesarChipher("Nick", 0)).toMatch("Nick");
 });
