5b7d7c9039024919267e3a175579f2d0c8eb29cf
Nick Haras <nick@macexperts.gr>
2021-03-20 19:13:21 +0200

    Add edge case test for analyze

--- analysis/analyze.test.js
+++ analysis/analyze.test.js
@@ -1,10 +1,19 @@
-import analyze from './analyze';
+import analyze from "./analyze";
 
-test('Should return proper analysis', () => {
+test("Should return proper analysis", () => {
   expect(analyze([1, 8, 3, 4, 2, 6])).toEqual({
     average: 4,
     min: 1,
     max: 8,
     length: 6,
+  });
+});
+
+test("Should return correct results for small arrays", () => {
+  expect(analyze([1])).toEqual({
+    average: 1,
+    min: 1,
+    max: 1,
+    length: 1,
   });
 });
