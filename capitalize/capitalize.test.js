2bce37296e293eed725259e3de5a7d176ac3d3d9
macnick <nick@macexperts.gr>
2021-09-22 09:51:31 +0300

    Fix the linter error

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -1,9 +1,7 @@
 import capitalize from './capitalize';
 
 test('The first letter of each word should be capital', () => {
-  expect(capitalize('welcome to javascript testing')).toMatch(
-    'Welcome To Javascript Testing'
-  );
+  expect(capitalize('welcome to javascript testing')).toMatch('Welcome To Javascript Testing');
 });
 
 test('It should remove spaces', () => {
